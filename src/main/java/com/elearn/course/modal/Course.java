c753dee849f25d90c0a5b6f0d577dd6eceecb8f9
Mehedi Hasan <mehedi08h@gmail.com>
2023-02-20 18:38:44 +0600

    project setup & create entity modal

---
+++ src/main/java/com/elearn/course/modal/Course.java
@@ -0,0 +1,23 @@
+package com.elearn.course.modal;
+
+import java.util.Objects;
+
+public class Course {
+    private Long courseId;
+    private String courseName;
+    private String courseDuration;
+    private String courseDescription;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Course course = (Course) o;
+        return courseId.equals(course.courseId) && Objects.equals(courseName, course.courseName) && Objects.equals(courseDuration, course.courseDuration) && Objects.equals(courseDescription, course.courseDescription);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(courseId, courseName, courseDuration, courseDescription);
+    }
+}
---
+++ src/test/java/com/elearn/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elearn.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
